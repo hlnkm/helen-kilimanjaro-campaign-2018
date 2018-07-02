910507938b074f255b4d4c6ced0247ea40d2c610
joeyklee <j@jk-lee.com>
2018-07-02 16:31:00 -0400

    temp fix due to cors failure

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ $(window).on('load', function() {
     // $( "#result" ).load( `https://cors-anywhere.herokuapp.com/${mjffUrl} .was-raised`);
 
     // https://cors-anywhere.herokuapp.com/
-    $.get(`https://cors-anywhere.herokuapp.com/${mjffUrl}`, function(data) {
+    $.get(`../data/mjff-page.html`, function(data) {
       
       // let body = $.parseHTML(data)
       let wasRaised = $(data).find(".was-raised").text()
